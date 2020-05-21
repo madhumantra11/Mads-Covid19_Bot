18ac8f68193f38d4f0d9b047363962698d14ba0c
Madhulata Baid <58360275+madhumantra11@users.noreply.github.com>
2020-05-21 15:29:38 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -172,12 +172,11 @@ def processRequest(req):
     print( "***Statewise Cases*** \n\n" + webhookresponse1 + "\n\n" )
     print( "***Statewise Cases*** \n\n" + webhookresponse2 + "\n\n" )
     print( "***Statewise Cases*** \n\n" + webhookresponse3 + "\n\n*******END********* \n" )
-    fulfillmentText = webhookresponse1 + "\n" + webhookresponse2 + "\n" + webhookresponse3 + "\n\n" + "Do you want me to share COVID-19 related information on your email id? Please chose from the below \n 1. Sure! \n 2. No. Thanks!"
+    #fulfillmentText = webhookresponse1 + "\n" + webhookresponse2 + "\n" + webhookresponse3 + "\n\n" + "Do you want me to share COVID-19 related information on your email id? Please chose from the below \n 1. Sure! \n 2. No. Thanks!"
     log.write_log( sessionID, "Indian State Cases", webhookresponse1, intent )
     return {
-        "fulfillmentText": fulfillmentText
+        "fulfillmentText": webhookresponse1 +webhookresponse2 +webhookresponse3
     }
-
 
 if __name__ == '__main__':
  port = int(os.getenv('PORT',5000))
